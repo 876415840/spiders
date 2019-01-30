f5aa36cd556e9c1df5b44b41a0e437b364a1d99d
mengqinghao <mengqinghao@finupgroup.com>
2019-01-30 18:41:15 +0800

    nothing

--- src/main/java/com/example/demo/controller/SpidersController.kt
+++ src/main/java/com/example/demo/controller/SpidersController.kt
@@ -1,9 +1,5 @@
 package com.example.demo.controller
 
-import com.alibaba.fastjson.JSON
-import com.example.demo.mapper.HouseInfoMapper
-import com.example.demo.service.RealtyDataService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,12 +15,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/spiders")
 class SpidersController {
 
-    @Autowired
-    lateinit var houseInfoMapper: HouseInfoMapper
-
-    @GetMapping("/realtyInfo/{code}")
+    @GetMapping("/realtyInfo")
     fun realtyInfo(@PathVariable("code") code : String): String {
-        var houseInfo = houseInfoMapper.getByCode(code)
-        return JSON.toJSONString(houseInfo)
+
+        return "nothing"
     }
 }
