de9e450ab240bf9c5744ca9548e5b14b2b5e2649
mengqinghao <mengqinghao@finupgroup.com>
2020-05-08 11:13:09 +0800

    配置SSL

---
+++ src/main/java/com/example/demo/config/connector/SslConnectorConfig.java
@@ -0,0 +1,46 @@
+package com.example.demo.config.connector;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description: SSL连接配置
+ * @Author MengQingHao
+ * @Date 2020/5/8 10:52 上午
+ */
+@Configuration
+public class SslConnectorConfig {
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(redirectConnector());
+        return factory;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(Http11NioProtocol.class.getName());
+        connector.setScheme("http");
+        connector.setPort(9527);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+}
