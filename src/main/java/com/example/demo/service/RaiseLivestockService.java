d5eedeebfb68b7622649c8fce9d720517569818a
mengqinghao <mengqinghao@finupgroup.com>
2020-10-26 16:18:41 +0800

    卖牛犊模式

--- src/main/java/com/example/demo/service/RaiseLivestockService.java
+++ src/main/java/com/example/demo/service/RaiseLivestockService.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.service.impl.SellAdultBullByRaiseCowBreed;
+import com.example.demo.service.impl.SellBullCalfByRaiseCowBreed;
+
+import java.util.Random;
 
 /**
  * 饲养牲畜
@@ -11,6 +14,22 @@ import com.example.demo.service.impl.SellAdultBullByRaiseCowBreed;
 public interface RaiseLivestockService {
 
     int MONTH_FOR_YEAR = 12;
+
+    /**
+     * 根据{@code months}获取周年
+     *
+     * @param months 指定月数
+     * @return int 整周年时返回对应年数，否则返回0
+     * @author stephen
+     * @date 2020/10/26 11:44 上午
+     */
+    default int getYear(int months) {
+        int year = 0;
+        if (months % MONTH_FOR_YEAR == 0) {
+            year = months / MONTH_FOR_YEAR;
+        }
+        return year;
+    }
 
     /**
      * 饲养
@@ -26,6 +45,7 @@ public interface RaiseLivestockService {
 
     public static void main(String[] args) {
         RaiseLivestockService raiseLivestockService = new SellAdultBullByRaiseCowBreed();
-        raiseLivestockService.raise(10, 15000, 6, 15);
+//        RaiseLivestockService raiseLivestockService = new SellBullCalfByRaiseCowBreed();
+        raiseLivestockService.raise(20, 15000, 6, 10);
     }
 }
--- src/main/java/com/example/demo/service/impl/SellBullCalfByRaiseCowBreed.java
+++ src/main/java/com/example/demo/service/impl/SellBullCalfByRaiseCowBreed.java
@@ -3,27 +3,27 @@ package com.example.demo.service.impl;
 import com.example.demo.service.AbstractRaiseCowService;
 
 /**
- * 饲养母牛生产 - 卖成年公牛模式
+ * 饲养母牛生产 - 卖公牛犊模式
  *
  * @author stephen
- * @date 2020/10/23 11:10 上午
+ * @date 2020/10/26 11:20 上午
  */
-public class SellAdultBullByRaiseCowBreed extends AbstractRaiseCowService {
-
+public class SellBullCalfByRaiseCowBreed extends AbstractRaiseCowService {
+    
     @Override
     protected int getBullRaiseCost() {
-        // 成本随时间是由低到高(均价400)
-        return 400;
+        return 300;
     }
 
     @Override
     protected int getBullSellPhase() {
-        return 18;
+        return 6;
     }
 
     @Override
     protected int getBullSellPrice() {
-        // 20年最低售价2W+
-        return 20000;
+        // 20年6月牛犊售价最低1W+
+        return 10000;
     }
+
 }
